1ededda0a7aad4391baabc60ba83fd0d2f99e4b6
lorikku <lorik.konjusevci@student.howest.be>
2019-11-21 20:02:17 +0100

    Updated package.json and webpack config for PHP compatiblity

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+//const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const postcssPresetEnv = require('postcss-preset-env');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
@@ -8,7 +8,7 @@ module.exports = (env, {mode}) => {
   console.log(mode);
   return {
     output: {
-      filename: '[name].[hash].js'
+      filename: 'script.js'
     },
     devServer: {
       overlay: true,
@@ -22,17 +22,6 @@ module.exports = (env, {mode}) => {
           use: {
             loader: 'babel-loader'
           }
-        },
-        {
-          test: /\.html$/,
-          use: [
-            {
-              loader: 'html-srcsets-loader',
-              options: {
-                attrs: [':src', ':srcset']
-              }
-            }
-          ]
         },
         {
           test: /\.(jpe?g|png|svg|webp)$/,
@@ -68,12 +57,8 @@ module.exports = (env, {mode}) => {
       ]
     },
     plugins: [
-      new HtmlWebPackPlugin({
-        template: './src/index.html',
-        filename: './index.html'
-      }),
       new MiniCssExtractPlugin({
-        filename: 'style.[contenthash].css'
+        filename: 'style.css'
       }),
       new OptimizeCSSAssetsPlugin(),
       new webpack.HotModuleReplacementPlugin()
