334b6115a5928ebe455410a7de2058bae0124db6
wotusay <wout.salembier@hotmail.com>
2019-11-22 10:19:57 +0100

    Added all index files

---
+++ src/view/planner/index.php
@@ -0,0 +1,9 @@
+<section class="planning__wrapper">
+<h2 class="subtitle">Eigen planning</h2>
+<p class="emptystate">Maak je eigen planning aan de hand van jouw gegevens.</p>
+<a class="button" href="">Maak een plan</a>
+</section>
+<section>
+<h2 class="subtitle">Template plans</h2>
+<a href="" class="template__plan bodybuilding">Bodybuilding</a>
+</section>
