02bfc0eded69edc976b5817bb62082828589e70c
wotusay <wout.salembier@hotmail.com>
2019-11-24 20:00:27 +0100

    Added input fields and links to the steps

--- src/view/planner/planner.php
+++ src/view/planner/planner.php
@@ -1,3 +1,30 @@
 <section class="planner">
-
+<div class="planning__wrapper">
+<ul class="steps">
+<li class="step"><a class="step__link" href="">1</a></li>
+<li class="step"><a class="step__link" href="">2</a></li>
+<li class="step"><a class="step__link" href="">3</a></li>
+</ul>
+<h2>choose the workouts</h2>
+<form action="" method="get">
+<div class="checkbox__wrapper">
+<label>Leg day
+<input id="legday" type="checkbox" class="checkbox__button"></label>
+<label>Cardio
+<input id="cardio" type="checkbox" class="checkbox__button"></label>
+<label>Bench
+<input id="bench" type="checkbox" class="checkbox__button"></label>
+<label>swole
+<input id="swole" type="checkbox" class="checkbox__button"></label>
+<label>bulking
+<input id="bulking" type="checkbox" class="checkbox__button"></label>
+<label>form
+<input id="form" type="checkbox" class="checkbox__button"></label>
+<label>plank
+<input id="plank" type="checkbox" class="checkbox__button"></label>
+<label>plateau
+<input id="plateau" type="checkbox" class="checkbox__button"></label>
+</div>
+</form>
+</div>
 </section>
