07a7dcaf37f93c41093416c1d82039f02d559338
wotusay <wout.salembier@hotmail.com>
2019-11-26 11:25:44 +0100

    Fixed the active state of the buttons

--- src/view/planner/planner.php
+++ src/view/planner/planner.php
@@ -10,41 +10,41 @@
 <div class="input__wrapper">
 <span>Choose a workout</span>
 <div class="checkbox__wrapper">
-<label for='legday'>Leg day</label>
 <input id="legday" type="checkbox" class="checkbox__button">
-<labe for='cardio'>Cardio</label>
+<label for='legday'>Leg day</label>
 <input id="cardio" type="checkbox" class="checkbox__button">
-<label for='bench'>Bench</label>
+<label for='cardio'>Cardio</label>
 <input id="bench" type="checkbox" class="checkbox__button">
-<label for='swole'>swole</label>
+<label for='bench'>Bench</label>
 <input id="swole" type="checkbox" class="checkbox__button">
-<label for='bulking'>bulking</label>
+<label for='swole'>swole</label>
 <input id="bulking" type="checkbox" class="checkbox__button">
-<label for='form'>form</label>
+<label for='bulking'>bulking</label>
 <input id="form" type="checkbox" class="checkbox__button">
-<label for='plank'>plank</label>
+<label for='form'>form</label>
 <input id="plank" type="checkbox" class="checkbox__button">
-<label for='plateau'>plateau</label>
+<label for='plank'>plank</label>
 <input id="plateau" type="checkbox" class="checkbox__button">
+<label for='plateau'>plateau</label>
 </div>
 </div>
 <div class="input__wrapper">
 <span>Choose the days</span>
 <div class="checkbox__wrapper">
-<label for='mon'>mon.</label>
 <input id="mon" type="checkbox" class="checkbox__button">
-<label for='tues'>tues.</label>
+<label for='mon'>mon.</label>
 <input id="tues" type="checkbox" class="checkbox__button">
-<label for='wens'>wens.</label>
+<label for='tues'>tues.</label>
 <input id="wens" type="checkbox" class="checkbox__button">
-<label for='thurs'>thurs.</label>
+<label for='wens'>wens.</label>
 <input id="thurs" type="checkbox" class="checkbox__button">
-<label for='fri'>fri.</label>
+<label for='thurs'>thurs.</label>
 <input id="fri" type="checkbox" class="checkbox__button">
-<label for='satur'>satur.</label>
+<label for='fri'>fri.</label>
 <input id="satur" type="checkbox" class="checkbox__button">
-<label for='sun'>sun.</label>
+<label for='satur'>satur.</label>
 <input id="sun" type="checkbox" class="checkbox__button">
+<label for='sun'>sun.</label>
 </div>
 </div>
 <input type="submit" value="Next step" class='button button-submit'>
