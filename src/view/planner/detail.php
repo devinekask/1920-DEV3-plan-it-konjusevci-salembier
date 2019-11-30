65d63a1a8c0bf6a267c49c1cd93024caec98c6b6
wotusay <wout.salembier@hotmail.com>
2019-11-30 19:30:30 +0100

    Added all html tags for the next step in the detail page

--- src/view/planner/detail.php
+++ src/view/planner/detail.php
@@ -1,4 +1,4 @@
-<section class='planning'>
+<section class='hidden planning planning--stap1'>
 <div class="information__wrapper">
 <h2>Title of workout</h2>
 <div class="personal__workout hidden">
@@ -13,18 +13,17 @@
   <p>time <span class="personal__time">30mins</span></p></div>
 </div>
 </div>
-<div class="template">
-<div class="template__wrapper">
+  <div class="template">
+    <div class="template__wrapper">
 <p class="information-template--workout">?</p>
-<div class="template--diffuclty">
+      <div class="template--diffuclty">
 <span>Diffuclty</span>
 <div class="diffuclty diffuclty--active"></div>
 <div class="diffuclty"></div>
 <div class="diffuclty"></div>
-</div>
-</div>
-</div>
-</div>
+      </div>
+    </div>
+  </div>
 </div>
 <div class="information-js hidden">Hey dit is een workt en dit de informatie ervan.</div>
 <div class="exercises">
@@ -61,5 +60,45 @@
 </ul>
 <div class="planning__wrapper">
 <a href="index.php?page=detail" class="button button-submit">Begin</a></div>
+</div>
+</section>
+
+<section class="planning planning--stap2">
+<div class="information__wrapper">
+<h2>Title of workout</h2>
+<div class="personal__workout hidden">
+<div class="personal">
+  <div class="personal__days">
+  <p>days completed <br> <span class="personal__numbers days">1 / 3</span></p>
+  </div>
+</div>
+<div class="time">
+<div class="personal-time">
+    <br>
+  <p>time <span class="personal__time">30mins</span></p></div>
+</div>
+</div>
+  <div class="template">
+    <div class="template__wrapper">
+<p class="information-template--workout">?</p>
+      <div class="template--diffuclty">
+<span>Diffuclty</span>
+<div class="diffuclty diffuclty--active"></div>
+<div class="diffuclty"></div>
+<div class="diffuclty"></div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="information-js hidden">Hey dit is een workt en dit de informatie ervan.</div>
+<div class="extended information">
+  <span class="excersices-left">1/10</span>
+  <a class="exit" href="index.php">+</a>
+</div>
+<div class="planning__wrapper">
+<img src="../assets/img/plank@2x.jpg" alt="plank" width="186">
+<span class="excersice-name">excersice-name</span>
+<span class="excersice-todo">x10</span>
+<a href="index.php?page=detail" class="button button-submit">Next</a></div>
 </div>
 </section>
