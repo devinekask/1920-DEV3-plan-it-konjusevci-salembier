f250bcc68027c94f5fb07b12eb5a6cfec7b50fe1
lorikku <lorik.konjusevci@student.howest.be>
2019-11-30 13:46:36 +0100

    Personal plans elements added to html and css

--- src/view/planner/index.php
+++ src/view/planner/index.php
@@ -1,15 +1,22 @@
 <section class="planning">
 <h2>Eigen planning</h2>
 <div class="planning__wrapper">
+<?php if(empty($workouts)){ ?>
     <p class="planning__emptystate">Maak je eigen planning aan de hand van jouw gegevens.</p>
     <a href="index.php?page=planner" class="button">Maak een planning</a>
+<?php }else{ ?>
+    <ul class="personal__plans">
+    <?php foreach($workouts as $workout): ?>
+        <li class="plan plan--personal"><a class="personalplan__link" href="">Bodybuilding</a></li>
+    <?php endforeach; } ?>
+    </ul>
 </div>
 </section>
 <section class="templateplan">
 <h2>Template plan</h2>
 <ul>
-<li class="templateplan__plan"><a class="templateplan__link" href="">Bodybuilding</a></li>
-<li class="templateplan__plan"><a class="templateplan__link" href="">Bodybuilding</a></li>
-<li class="templateplan__plan"><a class="templateplan__link" href="">Bodybuilding</a></li>
+<li class="plan plan--template"><a class="templateplan__link" href="">Bodybuilding</a></li>
+<li class="plan plan--template"><a class="templateplan__link" href="">Bodybuilding</a></li>
+<li class="plan plan--template"><a class="templateplan__link" href="">Bodybuilding</a></li>
 </ul>
 </section>
