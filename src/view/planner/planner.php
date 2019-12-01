e31d55278345abb59bcc2990d3d10903d46efabf
wotusay <wout.salembier@hotmail.com>
2019-12-01 18:16:35 +0100

    added new css file and new vars, changed langues to nl

--- src/view/planner/planner.php
+++ src/view/planner/planner.php
@@ -10,7 +10,7 @@
       </ul>
       <h2 class='hidden'>Planning</h2>
       <div class="input__wrapper">
-        <span>Choose a workout</span>
+        <span>Kies een workout</span>
         <div class="checkbox__wrapper">
           <input id="legday" name="workouts[]" value="leg day" type="checkbox" class="checkbox__button">
           <label for='legday'>Leg day</label>
@@ -31,22 +31,22 @@
         </div>
       </div>
       <div class="input__wrapper">
-        <span>Choose the days</span>
+        <span>Kies de dagen</span>
         <div class="checkbox__wrapper">
           <input id="mon" name="days[]" value="monday" type="checkbox" class="checkbox__button">
-          <label for='mon'>mon.</label>
+          <label for='mon'>maan.</label>
           <input id="tues" name="days[]" value="tuesday" type="checkbox" class="checkbox__button">
-          <label for='tues'>tues.</label>
+          <label for='tues'>dins.</label>
           <input id="wens" name="days[]" value="wednesday" type="checkbox" class="checkbox__button">
-          <label for='wens'>wednes.</label>
+          <label for='wens'>woens.</label>
           <input id="thurs" name="days[]" value="thursday" type="checkbox" class="checkbox__button">
-          <label for='thurs'>thurs.</label>
+          <label for='thurs'>donder.</label>
           <input id="fri" name="days[]" value="friday" type="checkbox" class="checkbox__button">
-          <label for='fri'>fri.</label>
+          <label for='fri'>vrij.</label>
           <input id="satur" name="days[]" value="satruday" type="checkbox" class="checkbox__button">
-          <label for='satur'>satur.</label>
+          <label for='satur'>zater.</label>
           <input id="sun" name="days[]" value="sunday" type="checkbox" class="checkbox__button">
-          <label for='sun'>sun.</label>
+          <label for='sun'>zon.</label>
         </div>
       </div>
       <div class="planning__wrapper"><input type="submit" value="Next step" class='button button-submit'></div>
@@ -65,17 +65,17 @@
       </ul>
       <h2 class='hidden'>Planning</h2>
       <div class="input__wrapper">
-        <span class="subtitle__input2">How long will this session take?</span>
+        <span class="subtitle__input2">Hoe lang wil dat de workout duurdt?</span>
         <div class="input__wrapper--step2">
           <input name="time" value="30" maxlength="3" size="3" readonly class="input__step2">
-          <p class="input__step2 input__step2--extra">Minutes</p>
+          <p class="input__step2 input__step2--extra">Minuten</p>
         </div>
         <div class="slider__wrapper">
           <input type="range" min="15" max="180" value="30" class="slider" id="time">
         </div>
       </div>
       <div class="input__wrapper">
-        <span class="subtitle__input2">How much do you weigh?</span>
+        <span class="subtitle__input2">Hoeveel weeg je?</span>
         <div class="input__wrapper--step2">
           <input size="3" name="weight" value="65" maxlength="3" readonly class="input__step2">
           <p class="input__step2 input__step2--extra">KG</p>
@@ -85,7 +85,7 @@
         </div>
       </div>
       <div class="input__wrapper">
-        <span class="subtitle__input2">How tall are you?</span>
+        <span class="subtitle__input2">Hoe groot ben je?</span>
         <div class="input__wrapper--step2">
           <input size="3" name="height" value="170" maxlength="3" readonly class="input__step2">
           <p class="input__step2 input__step2--extra">CM</p>
@@ -95,10 +95,10 @@
         </div>
       </div>
       <div class="input__wrapper">
-        <span class="subtitle__input2">What's your age?</span>
+        <span class="subtitle__input2">Wat is je leeftijd?</span>
         <div class="input__wrapper--step2">
           <input size="2" name="age" value="19" maxlength="2" readonly class="input__step2">
-          <p class="input__step2 input__step2--extra">Y/O</p>
+          <p class="input__step2 input__step2--extra">J/O</p>
         </div>
         <div class="slider__wrapper">
           <input type="range" min="15" max="75" value="19" class="slider" id="age">
@@ -120,10 +120,10 @@
             data-planner-class="planning planning--stap3" href="">3</a></li>
         <li class="step"><a class="step__link" data-planner-class="planning planning--stap4" href="">4</a></li>
       </ul>
-      <h2 class="planner__title">LIST OF EXERCISES</h2>
+      <h2 class="planner__title">lijst van oefeningen</h2>
       <div class="exercises__wrapper">
         <div class="input__wrapper doing__wrapper">
-          <span>want to do</span>
+          <span>wil ik doen</span>
           <input checked id='running' type="checkbox" class="checkbox__button">
           <label class="doing" for="running">running</label>
           <input checked id='cardio1' type="checkbox" class="checkbox__button">
@@ -134,7 +134,7 @@
           <label class="doing" for="walking">walking</label>
         </div>
         <div class="input__wrapper not__doing">
-          <span class="notdoing">not doing</span>
+          <span class="notdoing">Doe ik niet</span>
           <input id='biking' type="checkbox" class="checkbox__button">
           <label for="biking">biking</label>
           <input id='hardrunning' type="checkbox" class="checkbox__button">
@@ -163,14 +163,14 @@
             data-planner-class="planning planning--stap4" href="">4</a></li>
       </ul>
       <div class="input__wrapper sporty__wrapper">
-        <span class="difficulty">CHOOSE YOUR DIFFICULTY</span>
+        <span class="difficulty">KIES JE MOEILIJKHEIDSGRAAD</span>
         <div class="radio__wrapper">
           <input type="radio" name="sporty" value="easy" id="easy" checked>
-          <label class="radio__button" for="easy">easy</label>
+          <label class="radio__button" for="easy">Makkelijk</label>
           <input type="radio" name="sporty" value="intermediate" id="intermediate">
-          <label class="radio__button" for="intermediate">intermediate</label>
+          <label class="radio__button" for="intermediate">Gemiddeld</label>
           <input type="radio" name="sporty" value="hard" id="hard">
-          <label class="radio__button" for="hard">hard</label>
+          <label class="radio__button" for="hard">Expert</label>
         </div>
       </div>
       <input type="hidden" name="action" value="insert">
