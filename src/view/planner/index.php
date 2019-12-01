a7f086a07dc6997ea7bec14ebbc5785ddf56690a
wotusay <wout.salembier@hotmail.com>
2019-12-01 17:46:49 +0100

    fixed bugg

--- src/view/planner/index.php
+++ src/view/planner/index.php
@@ -63,7 +63,7 @@
     <h2>Template plan</h2>
     <ul class="planning__wrapper">
       <?php foreach($templateworkouts as $templateworkout): ?>
-        <li class="template"><img class="template__img" src="<?php echo $templateworkout['picturemobile']; ?>" alt="<?php echo $templateworkout['title']; ?>"><a class="templateplan__link" href="index.php?page=detail&id=<?php echo $templateworkout['id'];?>">
+        <li class="template"><img class="template__img" src="<?php echo $templateworkout['picturemobile']; ?>" alt="<?php echo $templateworkout['title']; ?>"><a class="templateplan__link" href="index.php?page=detailTemplate&id=<?php echo $templateworkout['id'];?>">
         <?php echo $templateworkout['title']; ?></a></li>
       <?php endforeach;?>
     </ul>
