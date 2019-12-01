5b3703d1fbc049ce2f6d0733a380ff4939b909fe
wotusay <wout.salembier@hotmail.com>
2019-12-01 22:25:58 +0100

    bug fixing for heroku

--- src/dao/DAO.php
+++ src/dao/DAO.php
@@ -11,12 +11,12 @@ class DAO {
 
 		if(empty(self::$sharedPDO)) {
 
-      $dbHost = getenv('PHP_DB_HOST') ?: "localhost";
-      $dbName = getenv('PHP_DB_DATABASE') ?: "workitout";
-      $dbUser = getenv('PHP_DB_USERNAME') ?: "workitoutuser";
-      $dbPass = getenv('PHP_DB_PASSWORD') ?: "workitout";
+      $dbHost = getenv('PHP_DB_HOST');
+      $dbName = getenv('PHP_DB_DATABASE');
+      $dbUser = getenv('PHP_DB_USERNAME');
+      $dbPass = getenv('PHP_DB_PASSWORD');
 
-			self::$sharedPDO = new PDO("mysql:host=" . self::$dbHost . ";dbname=" . self::$dbName, self::$dbUser, self::$dbPass);
+			self::$sharedPDO = new PDO("mysql:host=" . $dbHost . ";dbname=" . $dbName, $dbUser, $dbPass);
 			self::$sharedPDO->exec("SET CHARACTER SET utf8");
 			self::$sharedPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			self::$sharedPDO->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
